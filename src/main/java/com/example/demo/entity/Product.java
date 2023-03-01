ef194de134887d6c7d55992e76f158c130b265f2
Sera <huent6501.tlu@gmail.com>
2023-03-01 09:16:11 +0700

    first

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Products")
+public class Product {
+    @javax.persistence.Id
+    @Column
+    private String Id;
+
+    @Column
+    private String Name;
+
+    public Product() {
+    }
+    public Product(String id, String name) {
+        Id = id;
+        Name = name;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
